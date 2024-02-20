cc49051e508986fa82325a818ee86e1277283cb3
etsxxx <ets.nkn@gmail.com>
2024-02-20 18:23:37 +0900

    feature: support GET query

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # go-simple-http-redirector
 
 Redirect HTTP requests to specific URI.
-This process supports multiple domains. If you want it to work with multiple domains, please point DNS or the reverse proxies to the same process.
+
+Redirect rules can be configured via CSV file. It suited for cases where a large number of redirect rules are required.
+
+This process supports redirect rules that determine the domains as well as paths. If you want it to work with multiple domains, please point DNS or the reverse proxies to the same process.
 
 ## Configurations
 
 Use CSV file.
 
 ```csv
-before,after
+https://before/hoge,https://after/yo
+https://before/hoge?a=1,https://after/yo?z=1
 ```
 
 The `before` and `after` strings should include the scheme.
 
 See [example](./configs/examples.csv).
+
+## About Redirect Rules
+
+- Return a location where the set of GET queries in the request exactly matches the set of queries in the redirect rule.
+  - Order of GET queries is not considered.
+- A redirect rule with no GET query specified becomes the default rule for that PATH.
+  - If the GET query of the request does not match any of the redirect rules, the location of the default rule is returned.
+  - If the default rule is not defined, the request does not match any of the redirect rules, status 404 returned.
+- Redirect rules are domain-aware.
+- If there are duplicate redirect rules, the first defined rule is returned.
+- Extra GET queries are removed on redirect.
--- cmd/redirector/main_test.go
+++ cmd/redirector/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -32,21 +34,86 @@ func TestParseCSV(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Call the parseCSV function with the temporary CSV file path
+	// Initialize
+	redirectRules = NewRedirectRules()
+
 	err = parseCSV(tmpfile.Name())
 	if err != nil {
 		t.Errorf("parseCSV returned an error: %v", err)
 	}
 
-	// Check the patterns map
-	expected := map[string]redirectPattern{
-		"before1/1":     {dest: "https://after1/dir/1"},
-		"before1/dir/2": {dest: "https://after1/dir/dir/2"},
-		"before2/1":     {dest: "https://after2/dir/1"},
+	// Check the rules map
+	expected := map[string]string{
+		"before1/1":     "https://after1/dir/1",
+		"before1/dir/2": "https://after1/dir/dir/2",
+		"before2/1":     "https://after2/dir/1",
 	}
 	for k, v := range expected {
-		if patterns[k] != v {
-			t.Errorf("patterns[%q] = %v, want %v", k, patterns[k], v)
+		if redirectRules.Rules[k][0].afterURL != v {
+			t.Errorf("rule[%q] = %v, want %v", k, redirectRules.Rules[k], v)
+		}
+	}
+}
+
+func TestRedirect(t *testing.T) {
+	redirectRules = NewRedirectRules()
+	if err := parseCSV("../../configs/examples.csv"); err != nil {
+		t.Fatal("parseCSV: ", err)
+	}
+
+	// Test for defined rules
+	expected := map[string]string{
+		"https://before/hoge":             "https://after/yo",
+		"https://before/hoge?a=1":         "https://after/yo?z=1",
+		"https://before/hoge?a=2":         "https://after/yo?z=2",
+		"https://before/hoge?b=2":         "https://after/yo?z=2",
+		"https://before/hoge?c=3":         "https://after/yo", // no match returns default
+		"https://before/hoge?a=1&b=2":     "https://after/yo?z=3",
+		"https://before/hoge?b=2&a=1":     "https://after/yo?z=3", // order of query parameters does not matter
+		"https://before/hoge?c=3&b=2&a=1": "https://after/yo?z=3", // no match query is ignored
+		"https://before/hoge?a=3&b=4":     "https://after/yo?z=7",
+		"https://before/hoge?a=3":         "https://after/yo", // no match returns default
+		"http://before/fuga":              "https://after/dir/hey",
+		"http://before/fuga?a=1":          "https://after/dir/hey",
+		"https://anotherdomain/hoge":      "https://another/yo",
+	}
+	for requestURL, expectedLocation := range expected {
+		req, err := http.NewRequest("GET", requestURL, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		redirect(rr, req)
+
+		// Check the response status code
+		if rr.Code != http.StatusMovedPermanently {
+			t.Errorf("expected status code %d, got %d", http.StatusMovedPermanently, rr.Code)
+		}
+		// Check the response header "Location"
+		if location := rr.Header().Get("Location"); location != expectedLocation {
+			t.Errorf("%s : expected location %s, got %s", requestURL, expectedLocation, location)
+		}
+	}
+
+	// Test for not defined rules
+	expected = map[string]string{
+		"https://before/notdefined": "",
+		"https://notdefined/hoge":   "",
+		"http://before/piyo":        "", // default rule is not defined for this path
+		"http://before/piyo?z=1":    "", // default rule is not defined for this path
+
+	}
+	for requestURL, _ := range expected {
+		req, err := http.NewRequest("GET", requestURL, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		redirect(rr, req)
+
+		// Check the response status code
+		if rr.Code != http.StatusNotFound {
+			t.Errorf("expected status code %d, got %d", http.StatusNotFound, rr.Code)
 		}
 	}
 }
